9b8179c6c9c077dba238fd6fcc845cdaa7fd6a6c
Mike South <msouth@gmail.com>
2005-08-14 00:08:10 +0000

    Import of MSOUTH/Math-MatrixReal-Ext1-0-06 from CPAN.
    gitpan-cpan-distribution: Math-MatrixReal-Ext1-0
    gitpan-cpan-version:      06
    gitpan-cpan-path:         MSOUTH/Math-MatrixReal-Ext1-0-06.tgz
    gitpan-cpan-author:       MSOUTH
    gitpan-cpan-maturity:     released

--- Ext1.pm
+++ Ext1.pm
@@ -4,140 +4,144 @@ use strict;
 use Math::MatrixReal;
 use Carp;
 
-
 use base qw/Math::MatrixReal/;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 sub new_from_cols {
-	my $proto = shift;
-	my $class = ref($proto) || $proto;
-	my $ref_to_cols = shift;
-	my @cols = @{$ref_to_cols};
+	my $this = shift;
+    my $extra_args = ( @_ > 1 && ref($_[-1]) eq 'HASH' ) ? pop : {};
+    $extra_args->{_type} = 'column';
 
-	my $cols = scalar( @cols );
+	return $this->_new_from_rows_or_cols(@_, $extra_args );
+}
+sub new_from_columns {
+    my $this = shift;
+    $this->new_from_cols(@_);
+}
+sub new_from_rows {
+	my $this = shift;
+    my $extra_args = ( @_ > 1 && ref($_[-1]) eq 'HASH' ) ? pop : {};
+    $extra_args->{_type} = 'row';
 
-	my $matrix = 0;
-	my $rows = 0;
-
-	# each arg is a column, but we don't know what form they're
-	# in yet
-
-	my $col_index = 0;
-	foreach my $col (@cols) {
-		# it's one-based
-		$col_index ++;
-		my $ref = ref( $col ) ;
-
-		if ( $ref eq '' ) {
-			# we hope this is a string
-			$col = $class->new_from_string( $col );
-		}
-		elsif ( $ref eq 'ARRAY' ) {
-			my @array = @$col;
-			my $length = scalar( @array );
-			$col = $class->new_from_string( '[ '. join( " ]\n[ ", @array) ." ]\n" );
-		}
-		elsif ( $ref ne 'HASH' and $col->isa('Math::MatrixReal') ) {
-			# it's already a Math::MatrixReal something,
-            # we don't need to do anything, it will all
-            # work out--but we need this branch so we don't hit the 
-            # error else below
-		}
-		else {
-			# we have no idea, error time!
-			croak __PACKAGE__."::new_from_cols(): sorry, I have no clue what you sent me!  I only know how to deal with array refs, strings, and things that inherit from Math::MatrixReal\n";
-		}
-		my ($length, $one) = $col->dim;
-		croak __PACKAGE__."::new_from_cols(): This isn't a column vector"
-			  unless ($one == 1) ;
-
-		# if we already have a height, check that this is the same
-		if ($rows) {
-			croak __PACKAGE__."::new_from_cols(): This column vector has $length elements and an earlier one had $rows"
-			  unless ($length == $rows) ;
-		}
-		# else, we have a new height FIXME maybe this should check for zero
-		else {
-			$rows = $length;
-		}
-
-		# create the matrix the first time through
-		unless ($matrix) {
-			$matrix = $class->new($rows, $cols);
-		}
-
-		foreach my $row_index (1..$rows){
-			my $value = $col->element($row_index, 1);
-			$matrix->assign($row_index, $col_index, $value);
-		}
-
-	}
-	return $matrix;
+	return $this->_new_from_rows_or_cols(@_, $extra_args );
 }
 
-sub new_from_rows {
+sub _new_from_rows_or_cols {
 	my $proto = shift;
 	my $class = ref($proto) || $proto;
-	my $ref_to_rows = shift;
-	my @rows = @{$ref_to_rows};
+	my $ref_to_vectors = shift;
 
-	my $rows = scalar( @rows );
+    # these additional args are internal at the moment, 
+    # but in the future the user could pass e.g. {pad=>1} to
+    # request padding
+    my $args = pop;
+    my $vector_type = $args->{_type};
+    die "Internal ".__PACKAGE__." error" unless $vector_type =~ /^(row|column)$/;
 
-	my $matrix = 0;
-	my $cols = 0;
+    # step back one frame because this private method is 
+    # not how the user called it
+    my $caller_subname = (caller(1))[3];
 
-	# each arg is a column, but we don't know what form they're
-	# in yet
+    # note--this die() could be inconvenient if someone had something
+    # really fancy that knew how to be dereffed as an array 
+    # (can you do that with a tied scalar?), but I'm not putting
+    # the rest of the world through an eval--they can just
+    # deref and pass a reference themselves.  If that ever happens
+    # we can add an arg to skip this check
+    croak "$caller_subname: need a reference to an array of ${vector_type}s" unless ref($ref_to_vectors) eq 'ARRAY';
+	my @vectors = @{$ref_to_vectors};
 
-	my $row_index = 0;
-	foreach my $row (@rows) {
-		# it's one-based
-		$row_index ++;
-		my $ref = ref( $row ) ;
+	my $matrix;
+
+    my $other_type = {row=>'column', column=>'row'}->{$vector_type};
+
+    my %matrix_dim = (
+        $vector_type => scalar( @vectors ), 
+        $other_type  => 0,  # we will correct this in a bit
+    );
+
+    # row and column indices are one based
+	my $current_vector_count = 1; 
+	foreach my $current_vector (@vectors) {
+		# dimension is one-based, so we're
+        # starting with one here and incrementing
+        # as we go.  The other dimension is fixed (for now, until
+        # we add the 'pad' option), and gets set later
+		my $ref = ref( $current_vector ) ;
 
 		if ( $ref eq '' ) {
-			# we hope this is a string
-			$row = $class->new_from_string( $row );
+			# we hope this is a properly formatted Math::MatrixReal string,
+            # but if not we just let the Math::MatrixReal die() do it's
+            # thing
+			$current_vector = $class->new_from_string( $current_vector );
 		}
 		elsif ( $ref eq 'ARRAY' ) {
-			my @array = @$row;
-			my $length = scalar( @array );
-			$row = $class->new_from_string( '[ '. join( " ", @array) ." ]\n" );
+			my @array = @$current_vector;
+			croak "$caller_subname: one $vector_type you gave me was a ref to an array with no elements" unless @array ;
+            # we need to create the right kind of string based on whether
+            # they said they were sending us rows or columns:
+            if ($vector_type eq 'row') {
+                $current_vector = $class->new_from_string( '[ '. join( " ", @array) ." ]\n" );
+            }
+            else {
+                $current_vector = $class->new_from_string( '[ '. join( " ]\n[ ", @array) ." ]\n" );
+            }
 		}
-		elsif ( $ref ne 'HASH' and $row->isa('Math::MatrixReal') ) {
-			# it's already a Math::MatrixReal something,
+		elsif ( $ref ne 'HASH' and $current_vector->isa('Math::MatrixReal') ) {
+			# it's already a Math::MatrixReal something.
             # we don't need to do anything, it will all
             # work out
 		}
 		else {
 			# we have no idea, error time!
-			croak __PACKAGE__."::new_from_rows(): sorry, I have no clue what you sent me!  I only know how to deal with array refs, strings, and things that inherit from Math::MatrixReal\n";
+			croak "$caller_subname: I only know how to deal with array refs, strings, and things that inherit from Math::MatrixReal\n";
 		}
-		my ($one, $length) = $row->dim;
-		croak __PACKAGE__."::new_from_rows(): This isn't a column vector"
-			  unless ($one == 1) ;
 
-		# if we already have a height, check that this is the same
-		if ($cols) {
-			croak __PACKAGE__."::new_from_rows(): This column vector has $length elements and an earlier one had $cols"
-			  unless ($length == $cols) ;
-		}
-		# else, we have a new width  FIXME maybe this should check for zero
-		else {
-			$cols = $length;
-		}
+        # starting now we know $current_vector isa Math::MatrixReal thingy
+		my @vector_dims = $current_vector->dim;
+
+        #die unless the appropriate dimension is 1
+		croak "$caller_subname: I don't accept $other_type vectors"
+            unless ($vector_dims[ $vector_type eq 'row' ? 0 : 1 ] == 1) ;
+
+        # the other dimension is the length of our vector
+        my $length =  $vector_dims[ $vector_type eq 'row' ? 1 : 0 ];
+
+        # set the "other" dimension to the length of this
+        # vector the first time through
+        $matrix_dim{$other_type} ||= $length;
+
+        # die unless length of this vector matches the first length
+        croak "$caller_subname: one $vector_type has [$length] elements and another one had [$matrix_dim{$other_type}]--all of the ${vector_type}s passed in must have the same dimension"
+			  unless ($length == $matrix_dim{$other_type}) ;
 
 		# create the matrix the first time through
-		unless ($matrix) {
-			$matrix = $class->new($rows, $cols);
-		}
+        $matrix ||= $class->new($matrix_dim{row}, $matrix_dim{column});
 
-		foreach my $col_index (1..$cols){
-			my $value = $row->element(1, $col_index);
-			$matrix->assign($row_index, $col_index, $value);
-		}
+        # step along the vector assigning the value of each element
+        # to the correct place in the matrix we're building
+        foreach my $element_index ( 1..$length ){
+            # args for vector assignment:
+            # initialize both to one and reset the correct
+            # one below
+            my ($v_r, $v_c) = (1,1);
 
+            # args for matrix assignment
+            my ($row_index, $column_index, $value);
+
+            if ($vector_type eq 'row') {
+                $row_index           = $current_vector_count;
+                $v_c = $column_index = $element_index;
+            }
+            else {
+                $v_r = $row_index    = $element_index;
+                $column_index        = $current_vector_count;
+            }
+            $value = $current_vector->element($v_r, $v_c);
+            $matrix->assign($row_index, $column_index, $value);
+        }
+        $current_vector_count ++ ;
 	}
 	return $matrix;
 }
@@ -195,8 +199,7 @@ Creates a new matrix given a reference to an array of any of the following:
 
 =item * references to arrays
 
-=item * strings properly formatted to create a column with Math::MatrixReal's 
-C<new_from_string> command
+=item * strings properly formatted to create a column with Math::MatrixReal's C<new_from_string> command
 
 =back
 
@@ -212,11 +215,11 @@ Creates a new matrix given a reference to an array of any of the following:
 
 =over 4
 
-=item *	row vectors ( 1 by n Math::MatrixReal matrices )
+=item * row vectors ( 1 by n Math::MatrixReal matrices )
 
-=item *	references to arrays
+=item * references to arrays
 
-=item *	strings properly formatted to create a row with Math::MatrixReal's C<new_from_string> command
+=item * strings properly formatted to create a row with Math::MatrixReal's C<new_from_string> command
 
 =back
 
